7924c8e225ff9a2d1587b801c14ac6de2f132849
hemalathareddy20 <hemalathareddy401@gmail.com>
2020-11-19 20:27:52 +0530

    Added: logic to flip coin multiple times and display no of times head and tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
 echo "Welcom of the flip coin simulation program being solved in the Master branch"
-random=$((RANDOM%2))
-echo "$random"
-if(($random==1))
-then
-     echo "Heads winner"
+read -p "enter a range to flipcoin for multiple times: " n
+while(( $n!=0 ))
+do
+    random=$((RANDOM%2))
+    echo "$random"
+    if(($random==1))
+    then
+        ((headcount++))
 
-else
-     echo "Tails winner"
-fi
+    else
+        ((tailcount++))
+    fi
+((n--))
+done
+echo "$headcount times Heads won"
+echo "$tailcount times Tails won"
+
